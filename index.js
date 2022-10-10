b065a971ef2dc95e6cefad74af2cfbbd3fbc4893
raflirahman <raflirahman375@gmail.com>
2022-10-10 10:22:03 +0700

    add tugas

---
+++ index.js
@@ -0,0 +1,19 @@
+function deretAngka(n) {
+    // tulis code jawabanmu di sini
+    var hasil = '';
+    for (var i = 1; i <= n; i++) {
+      if (i % 3 === 0) {
+        hasil += 'NIO '
+      } else if (i % 5 === 5) {
+        hasil += 'MIC '
+      } else {
+        hasil += i + ' '
+      }
+  
+    }return hasil
+  }
+  
+  
+  console.log(deretAngka(10))
+  console.log(deretAngka(20))
+  console.log(deretAngka(30))
